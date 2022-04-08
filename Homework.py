04f4ac24dce6de576a0afd75a08a1216277edfc8
Viet7501 <94689163+Viet7501@users.noreply.github.com>
2022-04-09 00:15:24 +0700

    Update Homework.py

--- Homework.py
+++ Homework.py
@@ -1,24 +1,29 @@
+from phone_gen import PhoneNumber
+import names
 
+phone_number = PhoneNumber("Great Britain")
+d = ['London', 'Madrid', 'New York', 'Boston', 'Paris', 'Hanoi', 'Barcelona', 'Mannheim', 'México D.F.', 'São Paulo', 'Sevilla', 'Frankfurt a.M.', 'San Francisco', 'Montréal', 'Buenos Aires']
+
+# 50 Customers
 for j in range(50):
     random_city = random.choice(d)
     number = phone_number.get_mobile()
     j = Customer(name=names.get_last_name(), contact_name=names.get_full_name(), contact_number=number, address=random_city)
     j.save()
 
+# 50 Employees
 for i in range(50):
     random_city = random.choice(d)
     i = Employee(name=names.get_full_name(), address=random_city)
     i.save()
 
-d = ['London', 'Madrid', 'New York', 'Boston', 'Paris', 'Hanoi', 'Barcelona', 'Mannheim', 'México D.F.', 'São Paulo', 'Sevilla', 'Frankfurt a.M.', 'San Francisco', 'Montréal', 'Buenos Aires']
+# 5-2
+Customer.objects.last(update(contact_number='954-640-7780'))
 
-for i in range(10):
-    i = Employee(name=names.get_full_name(), address=random_address.real_random_address_by_state('CA'))
-    i.save()
+
+# 5-3
 
 Customer.objects.get_or_create(contact_name='Louis Huang')
-
-Customer.objects.last(update(contact_number='954-640-7780'))
 
 Customer.objects.create(name="Cattleman's Ranch", contact_name="Louis Huang", contact_number='954-640-7769',address='Orlando')
 
