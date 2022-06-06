6a540d08c0d92add4614a798af2a96c1fafa868f
Viet7501 <lequocviet07052001@gmail.com>
2022-06-06 21:47:28 +0700

    May 27, 2022

--- Lesson/note.py
+++ Lesson/note.py
@@ -1,13 +1,25 @@
-import datetime
-STATUS_NEW = 0
-STATUS_ACCEPTED = 1
+# import datetime
+# STATUS_NEW = 0
+# STATUS_ACCEPTED = 1
+#
+# STATUS_CHOICES = {
+#     (STATUS_NEW, 'New'),
+#     (STATUS_ACCEPTED, 'Accepted'),
+# }
+#
+# STATUS_CHOICES_DICT = dict(STATUS_CHOICES)
+# print(STATUS_CHOICES_DICT)
+# print(type(STATUS_CHOICES))
+# print(STATUS_CHOICES_DICT[0])
 
-STATUS_CHOICES = {
-    (STATUS_NEW, 'New'),
-    (STATUS_ACCEPTED, 'Accepted'),
-}
-
-STATUS_CHOICES_DICT = dict(STATUS_CHOICES)
-print(STATUS_CHOICES_DICT)
-print(type(STATUS_CHOICES))
-print(STATUS_CHOICES_DICT[0])
+# s = 0
+# while True:
+#     n = input('Vui lòng nhập n:')
+#     if n == 'stop':
+#         print("Bạn đã gửi")
+#         break
+#     else:
+#         if n.lstrip('-').isdigit():
+#             s += int(n)
+#
+# print(s)
